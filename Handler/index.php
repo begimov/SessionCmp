79191aa4d1061894b9de5a21867dbfa60d3e5426
begimov <begimov@gmail.com>
2017-07-28 14:14:41 +0300

    DB Session Handler implemented

--- Handler/index.php
+++ Handler/index.php
@@ -8,33 +8,84 @@ try {
 
 class DatabaseSessionHandler implements SessionHandlerInterface
 {
+    protected $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
     public function read($id)
     {
-        # code...
+        $statement = $this->db->preapre("
+            SELECT data FROM sessions
+            WHERE id = :id
+        ");
+
+        $statement->execute(['id' => $id]);
+
+        if ($row = $statement->fetch(PDO::FETCH_OBJ)) {
+            return $row->data;
+        }
+        return '';
     }
+
     public function write($id, $data)
     {
-        # code...
+        $statement = $this->db->prepare("
+            REPLACE INTO sessions VALUES (:id, :timestamp, :data)
+        ");
+
+        $insert = $statement->execute([
+            'id' => $id,
+            'timestamp' => time(),
+            'data' => $data
+        ]);
+
+        return ($insert) ? true : false;
     }
+
     public function open($path, $name)
     {
-        # code...
+        return ($this->db) ? true : false;
     }
+
     public function close()
     {
-        # code...
+        $this->db = null;
+
+        return ($this->db === null) ? true : false;
     }
+
     public function destroy($id)
     {
-        # code...
+        $statement = $this->db->prepare(["
+            DELETE FROM sessions WHERE id = :id
+        "]);
+
+        $delete = $statement->execute(['id' => $id]);
+
+        return ($delete) ? true : false;
     }
-    public function gc($maxlifetime )
+
+    public function gc($maxlifetime)
     {
-        # code...
+        $limit = time() - $maxlifetime;
+
+        $statement = $this->db->prepare("
+            DELETE FROM sessions WHERE access < :limit
+        ");
+
+        $delete = $statement->execute(['limit' => $limit]);
+
+        return ($delete) ? true : false;
     }
 }
 
-session_set_save_handler(new DatabaseSessionHandler);
+session_set_save_handler(new DatabaseSessionHandler($db));
+
 session_start();
 
 $_SESSION['name'] = 'Name';
+
+echo $_SESSION['name'];
